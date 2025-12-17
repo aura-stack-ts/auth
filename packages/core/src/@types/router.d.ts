2c25d5a5a6fbe451d3f01a7f3ae8c5735373d724
Hernan Alvarado <hernanvid123@gmail.com>
2025-12-17 12:38:35 -0500

    docs(web): update `API Reference` docs (#37)
    * docs(web): update `API Reference` docs
    
    * docs: add `guides` folder
    
    * docs: update `meta.json` file
    
    * docs(web): add `env.mdx` file
    
    * docs(web): add `Jose Utilities Guide`

--- packages/core/src/@types/router.d.ts
+++ packages/core/src/@types/router.d.ts
@@ -1,11 +1,11 @@
 import { createJoseInstance } from "@/jose.ts"
 import { createBuiltInOAuthProviders } from "@/oauth/index.ts"
 import type { GlobalContext } from "@aura-stack/router"
-import type { AuthConfig, CookieOptions } from "./index.ts"
+import type { AuthConfig, JoseInstance } from "./index.ts"
 
 declare module "@aura-stack/router" {
     interface GlobalContext extends Required<Omit<AuthConfig, "secret" | "oauth">> {
         oauth: ReturnType<typeof createBuiltInOAuthProviders>
-        jose: ReturnType<typeof createJoseInstance>
+        jose: JoseInstance
     }
 }
