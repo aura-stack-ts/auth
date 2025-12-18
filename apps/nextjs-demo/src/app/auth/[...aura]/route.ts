7b46b86889cb1422a5c64cdcf1c200c64b867de0
Hernan Alvarado <hernanvid123@gmail.com>
2025-12-18 10:25:42 -0500

    chore: update auth.ts config file

--- apps/nextjs-demo/src/app/auth/[...aura]/route.ts
+++ apps/nextjs-demo/src/app/auth/[...aura]/route.ts
@@ -1,12 +1,5 @@
-import { createAuth } from "@aura-stack/auth"
+import { auth } from "@/auth"
 
-const auth = createAuth({
-    oauth: ["github"],
-    secret: process.env.AURA_AUTH_SECRET ?? process.env.NEXT_PUBLIC_AURA_AUTH_SECRET,
-})
-
-export const GET = async (request: Request) => {
-    //return auth.handlers.GET(request)
-    console.log("GET /auth/[...aura]", auth.handlers)
-    return Response.json({ message: "Not implemented" })
-}
+export const {
+    handlers: { GET, POST },
+} = auth
--- apps/nextjs-demo/src/auth.ts
+++ apps/nextjs-demo/src/auth.ts
@@ -1,9 +1,10 @@
 import { createAuth } from "@aura-stack/auth"
-//import { builtInOAuthProviders } from "@aura-stack/auth/oauth/index"
+import { builtInOAuthProviders } from "@aura-stack/auth/oauth/index"
 
-//export const oauth = Object.keys(builtInOAuthProviders) as Array<keyof typeof builtInOAuthProviders>
+export const oauth = Object.keys(builtInOAuthProviders) as Array<keyof typeof builtInOAuthProviders>
 
 export const auth = createAuth({
-    oauth: ["github"],
+    oauth,
+    secret: process.env.AURA_AUTH_SECRET ?? process.env.NEXT_PUBLIC_AURA_AUTH_SECRET,
     trustedProxyHeaders: true,
 })
