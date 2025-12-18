f6f9094875ecd3bfb9b0aafca119cad8068c12a5
Hernan Alvarado <hernanvid123@gmail.com>
2025-12-18 08:33:06 -0500

    chore(route)!: add console.log to track behavior

--- apps/nextjs-demo/src/app/auth/[...aura]/route.ts
+++ apps/nextjs-demo/src/app/auth/[...aura]/route.ts
@@ -1,7 +1,8 @@
 // Is this the error ?
-//import { handlers } from "@/auth"
+import { handlers } from "@/auth"
 //export const { GET, POST } = handlers
 
-export const GET = async () => {
+export const GET = async (req: Request) => {
+    console.log("handlers: ", handlers, "GET: ", await handlers?.GET(req))
     return Response.json({ message: "Auth route is not configured." }, { status: 501 })
 }
