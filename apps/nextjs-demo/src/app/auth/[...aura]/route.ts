9692e425d14b902e114217606e2075af2b5a35a6
Hernan Alvarado <hernanvid123@gmail.com>
2025-12-18 08:25:14 -0500

    chore: add route

--- apps/nextjs-demo/src/app/auth/[...aura]/route.ts
+++ apps/nextjs-demo/src/app/auth/[...aura]/route.ts
@@ -1,3 +1,7 @@
 // Is this the error ?
 //import { handlers } from "@/auth"
 //export const { GET, POST } = handlers
+
+export const GET = async () => {
+    return Response.json({ message: "Auth route is not configured." }, { status: 501 })
+}
