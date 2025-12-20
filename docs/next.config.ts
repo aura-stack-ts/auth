e246d231c0baa955a28fa9884604ecf047200888
Hernan Alvarado <hernanvid123@gmail.com>
2025-12-20 12:24:12 -0500

    chore(web): add page actions

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -5,6 +5,14 @@ const withMDX = createMDX()
 
 const config: NextConfig = {
     reactStrictMode: true,
+    async rewrites() {
+    return [
+      {
+        source: '/docs/:path*.mdx',
+        destination: '/llms.mdx/:path*',
+      },
+    ];
+  },
 }
 
 export default withMDX(config)
