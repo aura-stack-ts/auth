fce16f73aab23b6e2253d1b4378b80ad846a7036
Hernan Alvarado <hernanvid123@gmail.com>
2026-01-18 14:38:43 -0500

    docs(oauth): add Mailchimp guide

--- packages/core/src/oauth/mailchimp.ts
+++ packages/core/src/oauth/mailchimp.ts
@@ -1,7 +1,26 @@
 import { OAuthProviderConfig } from "@/@types/index.js"
 
-export interface MailchimpProfile {}
+export interface Login {
+    email: string
+    avatar: string | null
+    login_id: number
+    login_name: string
+    login_email: string
+}
 
+export interface MailchimpProfile {
+    dc: string
+    role: string
+    accountname: string
+    user_id: string
+    login: Login
+    login_url: string
+    api_endpoint: string
+}
+
+/**
+ * @see [Mailchimp - Access Data on Behalf of Other Users with OAuth 2](https://mailchimp.com/developer/marketing/guides/access-user-data-oauth-2/)
+ */
 export const mailchimp: OAuthProviderConfig<MailchimpProfile> = {
     id: "mailchimp",
     name: "Mailchimp",
@@ -10,4 +29,12 @@ export const mailchimp: OAuthProviderConfig<MailchimpProfile> = {
     userInfo: "https://login.mailchimp.com/oauth2/metadata",
     scope: "",
     responseType: "code",
+    profile(profile) {
+        return {
+            sub: profile.user_id,
+            name: profile.accountname,
+            email: profile.login.login_email,
+            image: null,
+        }
+    },
 }
