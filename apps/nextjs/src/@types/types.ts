462ff1276730fd63137d962fbf012ab4f2e89811
Hernan Alvarado <hernanvid123@gmail.com>
2026-01-26 18:15:24 -0500

    chore: reduce code removing hero.tsx file

--- apps/nextjs/src/@types/types.ts
+++ apps/nextjs/src/@types/types.ts
@@ -1,10 +1,10 @@
-import { signIn, signOut } from "@/lib/client"
+import { createAuthClient } from "@/lib/client"
 import type { Session } from "@aura-stack/auth"
 
 export interface AuthContextValue {
     session: Session | null
     isLoading: boolean
     isAuthenticated: boolean
-    signIn: typeof signIn
-    signOut: typeof signOut
+    signIn: ReturnType<typeof createAuthClient>["signIn"]
+    signOut: ReturnType<typeof createAuthClient>["signOut"]
 }
