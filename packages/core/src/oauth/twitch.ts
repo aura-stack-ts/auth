da9af736364e8812f58823fe899794c556da2492
Hernan Alvarado <hernanvid123@gmail.com>
2026-03-02 10:38:31 -0500

    feat(oauth): update OAuth implementation

--- packages/core/src/oauth/twitch.ts
+++ packages/core/src/oauth/twitch.ts
@@ -4,7 +4,19 @@ import type { OAuthProviderCredentials } from "@/@types/index.ts"
 /**
  * @see [Twitch - Get Users](https://dev.twitch.tv/docs/api/reference#get-users)
  */
-export interface TwitchProfile {}
+export interface TwitchProfile {
+    id: string
+    login: string
+    display_name: string
+    type: string
+    broadcaster_type: string
+    description: string
+    profile_image_url: string
+    offline_image_url: string
+    view_count: number
+    email?: string
+    created_at: string
+}
 
 /**
  * @see [Twitch - Get Started with the Twitch API](https://dev.twitch.tv/docs/api/get-started/)
@@ -19,21 +31,28 @@ export const twitch = (options?: Partial<OAuthProviderCredentials<TwitchProfile>
     return {
         id: "twitch",
         name: "Twitch",
-        authorize: "https://id.twitch.tv/oauth2/authorize",
-        authorizeURL: "https://id.twitch.tv/oauth2/authorize",
+        authorize: {
+            url: "https://id.twitch.tv/oauth2/authorize",
+            params: { scope: "user:read:email", response_type: "code" },
+        },
         accessToken: "https://id.twitch.tv/oauth2/token",
         userInfo: {
             url: "https://api.twitch.tv/helix/users",
             headers: {
-                "Client-ID": getEnv("TWITCH_CLIENT_ID")
-            }
+                "Client-ID": getEnv("TWITCH_CLIENT_ID"),
+            },
         },
-        scope: "user:read:email",
-        responseType: "code",
-        profile(profile) {
+        profile(profile: { data: TwitchProfile[] }) {
             console.log("Twitch profile data:", profile)
+            const user = profile.data[0]
+            if (!user) {
+                throw new Error("No user data found in Twitch profile response")
+            }
             return {
-                sub: "",
+                sub: user.id,
+                name: user.display_name,
+                email: user.email,
+                picture: user.profile_image_url,
             }
         },
         ...options,
