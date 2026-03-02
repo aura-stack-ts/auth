294e26836f0d5c83f232393d8f1e65dd1f13aadf
Hernan Alvarado <hernanvid123@gmail.com>
2026-03-02 16:20:05 -0500

    chore(oauth): sync atlassian implementation

--- packages/core/src/oauth/atlassian.ts
+++ packages/core/src/oauth/atlassian.ts
@@ -1,4 +1,4 @@
-import type { LiteralUnion, OAuthProviderConfig } from "@/@types/index.js"
+import type { LiteralUnion, OAuthProviderCredentials } from "@/@types/index.js"
 
 export interface ExtendedProfile {
     job_title: string
@@ -10,14 +10,17 @@ export interface ExtendedProfile {
 export interface AtlassianProfile {
     account_id: string
     account_type: string
+    account_status: LiteralUnion<"active">
     email: string
+    email_verified: boolean
     name: string
     picture: string
-    account_status: LiteralUnion<"active">
     nickname: string
     zoneinfo: string
     locale: string
     extended_profile: ExtendedProfile
+    last_updated: string
+    created_at: string
 }
 
 /**
@@ -25,20 +28,33 @@ export interface AtlassianProfile {
  * @see [Atlassian - My Apps](https://developer.atlassian.com/console/myapps/)
  * @see [Atlassian - Retrieve Authenticated User](https://developer.atlassian.com/cloud/jira/software/oauth-2-3lo-apps/#how-do-i-retrieve-the-public-profile-of-the-authenticated-user-)
  */
-export const atlassian: OAuthProviderConfig<AtlassianProfile> = {
-    id: "atlassian",
-    name: "Atlassian",
-    authorizeURL: "https://auth.atlassian.com/authorize",
-    accessToken: "https://auth.atlassian.com/oauth/token",
-    userInfo: "https://api.atlassian.com/me",
-    scope: "read:me read:account",
-    responseType: "code",
-    profile(profile) {
-        return {
-            sub: profile.account_id,
-            name: profile.name,
-            email: profile.email,
-            image: profile.picture,
-        }
-    },
+export const atlassian = (
+    options?: Partial<OAuthProviderCredentials<AtlassianProfile>>
+): OAuthProviderCredentials<AtlassianProfile> => {
+    return {
+        id: "atlassian",
+        name: "Atlassian",
+        authorize: {
+            url: "https://auth.atlassian.com/authorize",
+            params: {
+                audience: "api.atlassian.com",
+                scope: "read:me read:account",
+                prompt: "consent",
+            },
+        },
+        authorizeURL: "https://auth.atlassian.com/authorize",
+        accessToken: "https://auth.atlassian.com/oauth/token",
+        userInfo: "https://api.atlassian.com/me",
+        scope: "read:me read:account",
+        responseType: "code",
+        profile(profile) {
+            return {
+                sub: profile.account_id,
+                name: profile.name,
+                email: profile.email,
+                image: profile.picture,
+            }
+        },
+        ...options,
+    } as OAuthProviderCredentials<AtlassianProfile>
 }
