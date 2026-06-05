835beb0bf055e0f0e2a0310bad52571bf34316ae
Hernan Alvarado <hernanvid123@gmail.com>
2026-06-05 16:03:01 -0500

    chore: fix type import

--- packages/integration/src/createAuth.ts
+++ packages/integration/src/createAuth.ts
@@ -1,4 +1,4 @@
-import { AuthConfig, createAuth as createAuthInstance } from "@aura-stack/auth"
+import { type AuthConfig, createAuth as createAuthInstance } from "@aura-stack/auth"
 import type { Identities } from "@aura-stack/auth/identity"
 
 export const createAuth = <Identity extends Identities>(config: AuthConfig<Identity>) => {
