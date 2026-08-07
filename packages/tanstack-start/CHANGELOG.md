86a30eb2b8357e989330a58ea39c492bfa4da953
Hernan Alvarado <hernanvid123@gmail.com>
2026-08-06 21:50:02 -0500

    feat(tanstack-start): Add Tanstack Start integration package

---
+++ packages/tanstack-start/CHANGELOG.md
@@ -0,0 +1,13 @@
+# Changelog - `@aura-stack/tanstack-start`
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+---
+
+## [Unreleased]
+
+### Added
+
+- Introduced the `@aura-stack/tanstack-start` integration package, delivering seamless authentication for TanStack Start applications across server-side rendering (SSR) and client-side rendering (CSR). Built on top of `@aura-stack/react`, the package provides first-class support for TanStack Start server functions, while client-side APIs are available through the `/client` entry point. [#255](https://github.com/aura-stack-ts/auth/pull/255)
---
+++ packages/tanstack-start/src/lib/api.ts
@@ -0,0 +1,221 @@
+import type {
+    TanstackStartSignInCredentialsOptions,
+    TanstackStartSignInCredentialsReturn,
+    TanstackStartSignInOptions,
+    TanstackStartSignInReturn,
+    TanstackStartSignOutOptions,
+    TanstackStartSignOutReturn,
+    TanstackStartUpdateSessionOptions,
+    TanstackStartUpdateSessionReturn,
+    TanstackStartSignUpOptions,
+    TanstackStartSignUpReturn,
+    TanstackStartGetProviderTokensOptions,
+    TanstackStartGetProviderTokensReturn,
+    TanstackStartGetAccessTokenOptions,
+    TanstackStartGetAccessTokenReturn,
+    TanstackStartRefreshUserInfoOptions,
+    TanstackStartRefreshUserInfoReturn,
+    TanstackStartRevokeTokenOptions,
+    TanstackStartRevokeTokenReturn,
+    TanstackStartDisconnectProviderOptions,
+    TanstackStartDisconnectProviderReturn,
+    TanstackStartIsProviderConnectedOptions,
+    TanstackStartIsProviderConnectedReturn,
+} from "@/@types/index.ts"
+import type { AuthInstance, User } from "@aura-stack/auth"
+import { createServerFn, type OptionalFetcher } from "@tanstack/react-start"
+import { getRequest, getRequestHeaders } from "@tanstack/react-start/server"
+
+export const getSession = <DefaultUser extends User = User>({ api }: AuthInstance<DefaultUser>) => {
+    return createServerFn({
+        method: "GET",
+        strict: true,
+    }).handler<any>(async () => {
+        const session = await api.getSession({
+            headers: getRequestHeaders(),
+        })
+        if (!session.success) return null
+        return session.session
+    }) as OptionalFetcher<undefined, undefined, Promise<DefaultUser | null>>
+}
+
+export const signIn = <DefaultUser extends User = User>({ api }: AuthInstance<DefaultUser>) => {
+    return createServerFn({ method: "POST" })
+        .validator((data: TanstackStartSignInOptions) => data)
+        .handler<Promise<TanstackStartSignInReturn>>(async ({ data }) => {
+            const { providerId, ...options } = data
+            const output = await api.signIn(providerId, {
+                ...options,
+                headers: getRequestHeaders(),
+                request: getRequest(),
+            })
+            const { headers: _h, toResponse: _t, ...spread } = output
+            return spread
+        })
+}
+
+export const signInCredentials = <DefaultUser extends User = User>({ api }: AuthInstance<DefaultUser>) => {
+    return createServerFn({ method: "POST" })
+        .validator((data: TanstackStartSignInCredentialsOptions) => data)
+        .handler<Promise<TanstackStartSignInCredentialsReturn>>(async ({ data }) => {
+            const output = await api.signInCredentials({
+                ...data,
+                headers: getRequestHeaders(),
+                request: getRequest(),
+            })
+            const { headers: _h, toResponse: _t, ...spread } = output
+            return spread
+        })
+}
+
+export const signOut = <DefaultUser extends User = User>({ api }: AuthInstance<DefaultUser>) => {
+    return createServerFn({ method: "POST" })
+        .validator((data: TanstackStartSignOutOptions = {}) => data)
+        .handler<Promise<TanstackStartSignOutReturn>>(async ({ data }) => {
+            const output = await api.signOut({
+                ...data,
+                headers: getRequestHeaders(),
+            })
+            const { headers: _h, toResponse: _t, ...spread } = output
+            return spread
+        })
+}
+
+export const updateSession = <DefaultUser extends User = User>({ api }: AuthInstance<DefaultUser>) => {
+    return createServerFn({ method: "POST" })
+        .validator((data: TanstackStartUpdateSessionOptions<DefaultUser>) => data)
+        .handler<Promise<TanstackStartUpdateSessionReturn<DefaultUser>>>(async ({ data }) => {
+            const output = await api.updateSession({
+                ...data,
+                headers: getRequestHeaders(),
+            })
+            const { headers: _h, toResponse: _t, ...spread } = output
+            return spread
+        })
+}
+
+export const signUp = <DefaultUser extends User = User>({ api }: AuthInstance<DefaultUser>) => {
+    return createServerFn({ method: "POST" })
+        .validator((data: TanstackStartSignUpOptions) => data)
+        .handler<Promise<TanstackStartSignUpReturn>>(async ({ data }) => {
+            const output = await api.signUp({
+                ...data,
+                headers: getRequestHeaders(),
+                request: getRequest(),
+            })
+            const { headers: _h, toResponse: _t, ...spread } = output
+            return spread
+        })
+}
+
+export const getProviderTokens = <DefaultUser extends User = User>({ api }: AuthInstance<DefaultUser>) => {
+    return createServerFn({ method: "GET" })
+        .validator((data: TanstackStartGetProviderTokensOptions) => data)
+        .handler<Promise<TanstackStartGetProviderTokensReturn>>(async ({ data }) => {
+            const { oauth, ...options } = data
+            const output = await api.getProviderTokens(oauth, {
+                ...options,
+                headers: getRequestHeaders(),
+            })
+            const { headers: _h, toResponse: _t, ...spread } = output
+            return spread
+        })
+}
+
+export const getAccessToken = <DefaultUser extends User = User>({ api }: AuthInstance<DefaultUser>) => {
+    return createServerFn({ method: "GET" })
+        .validator((data: TanstackStartGetAccessTokenOptions) => data)
+        .handler<Promise<TanstackStartGetAccessTokenReturn>>(async ({ data }) => {
+            const { oauth, ...options } = data
+            const output = await api.getAccessToken(oauth, {
+                ...options,
+                headers: getRequestHeaders(),
+            })
+            const { headers: _h, toResponse: _t, ...spread } = output
+            return spread
+        })
+}
+
+createServerFn({
+    method: "GET",
+}).handler(() => {
+    return {
+        name: "jose",
+        data: {
+            id: 1,
+        },
+    }
+})
+
+export const refreshUserInfo = <DefaultUser extends User = User>({ api }: AuthInstance<DefaultUser>) => {
+    return createServerFn({ method: "POST" })
+        .validator((data: TanstackStartRefreshUserInfoOptions) => data)
+        .handler<any>(async ({ data }) => {
+            const { oauth, ...options } = data
+            const output = await api.refreshUserInfo(oauth, {
+                ...options,
+                headers: getRequestHeaders(),
+            })
+            const { headers: _h, toResponse: _t, ...spread } = output
+            return spread
+        }) as OptionalFetcher<any, any, Promise<TanstackStartRefreshUserInfoReturn<DefaultUser>>>
+}
+
+export const revokeToken = <DefaultUser extends User = User>({ api }: AuthInstance<DefaultUser>) => {
+    return createServerFn({ method: "POST" })
+        .validator((data: TanstackStartRevokeTokenOptions) => data)
+        .handler<Promise<TanstackStartRevokeTokenReturn>>(async ({ data }) => {
+            const { oauth, ...options } = data
+            const output = await api.revokeToken(oauth, {
+                ...options,
+                headers: getRequestHeaders(),
+            })
+            const { headers: _h, toResponse: _t, ...spread } = output
+            return spread
+        })
+}
+
+export const disconnectProvider = <DefaultUser extends User = User>({ api }: AuthInstance<DefaultUser>) => {
+    return createServerFn({ method: "POST" })
+        .validator((data: TanstackStartDisconnectProviderOptions) => data)
+        .handler<Promise<TanstackStartDisconnectProviderReturn>>(async ({ data }) => {
+            const { oauth, ...options } = data
+            const output = await api.disconnectProvider(oauth, {
+                ...options,
+                headers: getRequestHeaders(),
+            })
+            const { headers: _h, toResponse: _t, ...spread } = output
+            return spread
+        })
+}
+
+export const isProviderConnected = <DefaultUser extends User = User>({ api }: AuthInstance<DefaultUser>) => {
+    return createServerFn({ method: "GET" })
+        .validator((data: TanstackStartIsProviderConnectedOptions) => data)
+        .handler<Promise<TanstackStartIsProviderConnectedReturn>>(async ({ data }) => {
+            const { oauth, ...options } = data
+            const output = await api.isProviderConnected(oauth, {
+                ...options,
+                headers: getRequestHeaders(),
+            })
+            const { headers: _h, toResponse: _t, ...spread } = output
+            return spread
+        })
+}
+
+export const api = <DefaultUser extends User = User>(config: AuthInstance<DefaultUser>) => {
+    return {
+        getSession: getSession<DefaultUser>(config),
+        signIn: signIn(config),
+        signInCredentials: signInCredentials(config),
+        signOut: signOut(config),
+        updateSession: updateSession(config),
+        signUp: signUp(config),
+        getProviderTokens: getProviderTokens(config),
+        getAccessToken: getAccessToken(config),
+        refreshUserInfo: refreshUserInfo(config),
+        revokeToken: revokeToken(config),
+        disconnectProvider: disconnectProvider(config),
+        isProviderConnected: isProviderConnected(config),
+    }
+}
